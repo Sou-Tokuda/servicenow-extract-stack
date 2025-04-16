a0aeaa2242c89968be093780f2e0e1669f46069e
Tokuda, Sou (SNC) <Sou.Tokuda@sony.com>
2025-04-16 16:39:20 +0900

    Initial commit

---
+++ test/servicenow-extract-stack.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ServicenowExtractStack from '../lib/servicenow-extract-stack-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/servicenow-extract-stack-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ServicenowExtractStack.ServicenowExtractStackStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
